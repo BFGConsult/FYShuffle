4f35eb61ce78de9eadf690f5de07fda36f196c77
BFG <Tom.Fredrik@blenning.no>
2025-07-11 03:33:18 +0200

    Add typescript support.

---
+++ dist/FYShuffle.d.ts
@@ -0,0 +1,4 @@
+export function FYForward(text: string, key: number): string;
+export function FYBackward(enc: string, key: number): string;
+export function genPerm(n: number, key: number): number[];
+export function nextRand(x: number): number;
